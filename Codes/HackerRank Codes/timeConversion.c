e165db8d8903ec8e72248ee6727202c01abead22
Shivang Mitra <shivangmitra8@gmail.com>
2020-09-06 19:44:52 +0530

    Completed Time Conversion Program

--- Codes/HackerRank Codes/timeConversion.c
+++ Codes/HackerRank Codes/timeConversion.c
@@ -2,11 +2,12 @@
 #include<string.h>
 #include<stdlib.h>
 
-char* timeConversion(char time[]);
+char* timeConversion(char *time);
 
 void main()
 {
-    char time[10];
+    char *time;
+    time=(char *)malloc(10*sizeof(char));
     scanf("%s",time);
 
     char *newTime;
@@ -14,21 +15,44 @@ void main()
 
     newTime=timeConversion(time);
 
+    for(int i=0;i<8;i++)
+    printf("%c",newTime[i]);
+
 }
 
-char* timeConversion(char time[])
+char* timeConversion(char *time)
 {
     char *newTime;
     newTime=(char*)malloc(8*sizeof(char));
 
-    if(time[8]=='A')
+    if((time[8]=='A' && time[0]!='1' && time[1]!='2') || (time[8]=='P' && time[0]=='1' && time[1]=='2'))
     {
         for(int i=0;i<8;i++)
         {
-            *(newTime+i)=time[i];
+            newTime[i]=time[i];
         }
     }
-    printf("%s",newTime);
-
+    else if(time[8]=='P')
+    {
+        for(int i=2;i<8;i++)
+        {
+            newTime[i]=time[i];
+        }
+        int hours;
+        hours=(int)(((time[0]-'0')*10)+(time[1]-'0'));
+        hours=hours+12;
+        newTime[1]=hours%10+'0';
+        hours=hours/10;
+        newTime[0]=hours+'0';
+    }
+    else
+    {
+        for(int i=2;i<8;i++)
+        {
+            newTime[i]=time[i];
+        }
+        newTime[0]=newTime[1]='0';
+    }
+    
     return newTime;
 }
