696c1287a22f40dbab81098d8d0d280361f5c94c
Shivang Mitra <shivangmitra8@gmail.com>
2020-09-10 20:38:16 +0530

    Did some more file handling

--- Codes/PythonCodes/playingWithFiles.py
+++ Codes/PythonCodes/playingWithFiles.py
@@ -1,4 +1,4 @@
-f = open("testText.txt", "rt") #rt is default
+# f = open("testText.txt", "rt")  rt is default
 # content = f.read()
 # print(content)
 
@@ -9,9 +9,21 @@ f = open("testText.txt", "rt") #rt is default
 # print(f.readline())
 # print(f.readline())
 
-print(f.readlines())
+# print(f.readlines())
+#this will real lines in the form of a list
 
 # content = f.read(3)
 # print(content)
+
+# f = open("testText.txt", "w") write mode
+# f = open("testText.txt", "a") append mode
+
+# f.write("Mai mast launda hu\n")
+# a = f.write("Mai mast launda hu\n")
+# print(a)
+
+f = open("testText.txt", "r+")
+print(f.read())
+f.write("Dhanyawaad")
 
 f.close()
