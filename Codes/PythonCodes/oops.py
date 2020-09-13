b26507f5dd9330563d3d2f0410a5916ef6d8e158
Shivang Mitra <shivangmitra8@gmail.com>
2020-09-13 21:50:39 +0530

    Static methods in Python

--- Codes/PythonCodes/oops.py
+++ Codes/PythonCodes/oops.py
@@ -9,8 +9,27 @@ class Employee:
     def printdetails(self):
         return f"Name is {self.name} - Salary is {self.salary} and role is {self.role}"
 
+#class methods
+
+    @classmethod
+    def change_leaves(cls, newleaves):
+        cls.no_of_leaves = newleaves
+
+#using class method as constructor
+    @classmethod
+    def from_dash(cls, string):
+        # params = string.split("-")
+        # return cls(params[0], params[1], params[2])
+        return cls(*string.split("-"))
+
+#static methods
+    @staticmethod
+    def printgood(string):
+        print("this is good " + string)
+
 harry = Employee("Harry", 455, "Instructor")
 rohan = Employee("Rohan", 45500, "CEO")
+karan = Employee.from_dash("Karan-480-Student")
 
 # harry.name = "Harry"
 # harry.salary = 455
@@ -20,5 +39,12 @@ rohan = Employee("Rohan", 45500, "CEO")
 # rohan.salary = 45500
 # rohan.role = "CEO"
 
-print(rohan.printdetails())
-print(harry.printdetails())
+# print(rohan.printdetails())
+# print(harry.printdetails())
+
+# rohan.change_leaves(56)
+# print(harry.no_of_leaves)
+
+# print(karan.salary)
+
+karan.printgood("Karan")
