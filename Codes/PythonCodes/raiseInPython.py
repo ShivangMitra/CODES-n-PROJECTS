73e44f6e9ba7475c5e60cbb82efb2889e86260c2
Shivang Mitra <shivangmitra8@gmail.com>
2020-09-16 17:59:21 +0530

    Completed Command Line Utility in Python

---
+++ Codes/PythonCodes/raiseInPython.py
@@ -0,0 +1,23 @@
+# a = input("What is your name\n")
+# b = input("How much do you earn\n")
+#
+# if int(b)==0:
+#     raise ZeroDivisionError("B is zero, stopping program")
+#
+# if a.isnumeric():
+#     raise Exception("Numbers are not allowed")
+#
+# print(f"Hello {a}")
+#
+# #1000 lines taking 1 hour
+
+# c = input("Enter your name\n")
+#
+# try:
+#     print(a)
+#
+# except Exception as e:
+#     if c =="harry":
+#         raise ValueError("HARRY IS BLOCKED")
+#
+#     print("EXCEPTION HANDLED")
