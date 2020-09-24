c6c3780508e8ced888d1b0615b2b9d9d7dcb7b44
Shivang Mitra <shivangmitra8@gmail.com>
2020-09-24 18:11:39 +0530

    Added colors in calculator

--- Projects/GUI Python/Calculator.py
+++ Projects/GUI Python/Calculator.py
@@ -26,14 +26,14 @@ def click(event):
 
 
 root = Tk()
-root.geometry("500x620")
+root.geometry("500x670")
 root.title("Z ka personal calculator")
 root.wm_iconbitmap("Calc.ico")
 root.configure(background="gray")
 
 scvalue = StringVar()
 scvalue.set("")
-screen = Entry(root, textvar=scvalue, font="lucida 40 bold")
+screen = Entry(root, textvar=scvalue, font="lucida 40 bold", relief=SUNKEN, borderwidth=10)
 screen.pack(pady=10, padx=10)
 
 lst = ["9", "8", "7", "+", "6", "5", "4", "-", "3", "2", "1", "/", "C", "0", "=", "*", ".", "%"]
@@ -42,12 +42,12 @@ frame = Frame(root, bg="gray")
 for item in lst:
     if lst.index(item)%4==0 and lst.index(item) is not 0:
         frame = Frame(root, bg="gray")
-        btn = Button(frame, text=item, padx=20, font="lucida 35 bold")
+        btn = Button(frame, text=item, padx=20, font="lucida 35 bold", background="orange", relief=RAISED, borderwidth=5)
         btn.pack(side=LEFT, padx=10, pady=5)
         btn.bind("<Button-1>", click)
         frame.pack()
     else:
-        btn = Button(frame, text=item, padx=20, font="lucida 35 bold")
+        btn = Button(frame, text=item, padx=20, font="lucida 35 bold", background="orange", relief=RAISED, borderwidth=5)
         btn.pack(side=LEFT, padx=10, pady=5)
         btn.bind("<Button-1>", click)
         frame.pack()
