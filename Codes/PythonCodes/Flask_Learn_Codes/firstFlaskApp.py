6fec272422bcce5e43d4d2e7aabb18a3dc36d45a
Shivang Mitra <shivangmitra8@gmail.com>
2020-10-10 19:57:39 +0530

    Pause a detectionOfCovid project and started learning Flask

---
+++ Codes/PythonCodes/Flask_Learn_Codes/firstFlaskApp.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/about")
+def helloZeph():
+    my_name = "Zephyrus"
+    return render_template("about.html", name=my_name)
+
+app.run(debug=True)
