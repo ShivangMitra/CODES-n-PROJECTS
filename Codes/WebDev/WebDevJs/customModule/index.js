4ce2633d0157c412089a0daefaa086f5a883efe2
Shivang Mitra <shivangmitra8@gmail.com>
2020-10-17 19:44:38 +0530

    Learned to create custom module in JS

---
+++ Codes/WebDev/WebDevJs/customModule/index.js
@@ -0,0 +1,10 @@
+// const average = require("./mod")
+// console.log(average([3,4]))
+
+
+//after creting a export object
+const mod = require("./mod")
+console.log(mod.avg([3,4]))
+console.log(mod.name)
+console.log(mod.repo)
+console.log("This is index")
