41ce1b22bdeec9fba10082c3f3e209f46bdde555
Shivang Mitra <shivangmitra8@gmail.com>
2020-10-19 18:19:00 +0530

    Created my first express app

---
+++ Codes/WebDev/WebDevJs/Express/app.js
@@ -0,0 +1,27 @@
+// Set-ExecutionPolicy RemoteSigned
+// Set-ExecutionPolicy Restricted
+
+const express = require("express")
+
+const app = express();
+const port = 80;
+
+app.get("/", (req, res)=>{
+    res.status(200).send("This is home page of my first express app")
+})
+
+app.get("/about", (req, res)=>{
+    res.send("This is about page of my first express app")
+})
+
+app.post("/about", (req, res)=>{
+    res.send("This is post request about page of my first express app")
+})
+
+app.get("/this", (req, res)=>{
+    res.status(404).send("This page is not found")
+})
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on the port ${port}`)
+})
