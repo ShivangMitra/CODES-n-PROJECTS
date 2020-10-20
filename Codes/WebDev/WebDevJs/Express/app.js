2e66798b2fc9aa0ec56a6c78457a9ca00aaf2003
Shivang Mitra <shivangmitra8@gmail.com>
2020-10-20 13:28:31 +0530

    Created a CSGO skins page, to learn to add backend with js

--- Codes/WebDev/WebDevJs/Express/app.js
+++ Codes/WebDev/WebDevJs/Express/app.js
@@ -7,35 +7,23 @@ const path = require("path")
 const app = express();
 const port = 80;
 
-// For serving static files
-app.use("/static", express.static("static"))
+// EXPRESS SPECIFIC STUFF
+app.use("/static", express.static("static")) // For serving static files
 
-// Set the template engine as pug
-app.set("view engine", "pug")
+// PUG SPECIFIC STUFF
+app.set("view engine", "pug") // Set the template engine as pug
+app.set("views", path.join(__dirname, "views")) // Set the views directory
 
-// Set the views directory
-app.set("views", path.join(__dirname, "views"))
-
-// Our pug demo endpoint
-app.get("/demo", (req, res)=>{
-    res.status(200).render("demo", { title: "Hey Zephyrus", message: "Hello there!"})
-})
-
+// ENDPOINTS
 app.get("/", (req, res)=>{
-    res.status(200).send("This is home page of my first express app")
+    const con = "Just fill in the form below"
+    const params = {
+        "title": 'CSGO is the best game',
+        "content": con
+    }
+    res.status(200).render("index.pug", params)
 })
 
-app.get("/about", (req, res)=>{
-    res.send("This is about page of my first express app")
-})
-
-app.post("/about", (req, res)=>{
-    res.send("This is post request about page of my first express app")
-})
-
-app.get("/this", (req, res)=>{
-    res.status(404).send("This page is not found")
-})
 
 app.listen(port, ()=>{
     console.log(`The application started successfully on the port ${port}`)
