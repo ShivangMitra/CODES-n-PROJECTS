7d3fdc9a6339b18cb9c92894d79e3764e7da3498
Shivang Mitra <shivangmitra8@gmail.com>
2020-10-20 14:31:20 +0530

    Applied backend in CSGO form and saved data in a file

--- Codes/WebDev/WebDevJs/Express/app.js
+++ Codes/WebDev/WebDevJs/Express/app.js
@@ -3,12 +3,14 @@
 
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
 
 const app = express();
 const port = 80;
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static")) // For serving static files
+app.use(express.urlencoded())
 
 // PUG SPECIFIC STUFF
 app.set("view engine", "pug") // Set the template engine as pug
@@ -20,6 +22,18 @@ app.get("/", (req, res)=>{
     const params = {
         "title": 'CSGO is the best game',
         "content": con
+    }
+    res.status(200).render("index.pug", params)
+})
+
+app.post("/", (req,res)=>{
+    form = req.body
+
+    let outputToWrite = `The Weapon is ${form.weapon}, the skin name is ${form.skin}, the required float is ${form.float}, client's favorite player is ${form.favplayer}, more about the client: ${form.more}\n`
+    fs.appendFileSync("output.txt", outputToWrite)
+
+    const params = {
+        "message": "Your form has been submitted successfully"
     }
     res.status(200).render("index.pug", params)
 })
