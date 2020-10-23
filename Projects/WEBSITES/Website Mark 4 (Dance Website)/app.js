afe0ebb4e538093edde2a22dbbaba1893cd1ef54
Shivang Mitra <shivangmitra8@gmail.com>
2020-10-23 19:30:31 +0530

    Connected Contacts page of Dance website to MongoDB

--- Projects/WEBSITES/Website Mark 4 (Dance Website)/app.js
+++ Projects/WEBSITES/Website Mark 4 (Dance Website)/app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const path = require("path");
-
+const bodyparser = require("body-parser")
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/contactDance", {useNewUrlParser: true, useUnifiedTopology: true});
+
 const port = 8000;
+
+
+// Defining mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+});
+
+var Contact = mongoose.model("Contact", contactSchema)
 
 // EXPRESS SPECIFIC STUFF
 app.use("/static", express.static("static")) // For serving static files
@@ -25,6 +41,16 @@ app.get("/contact", (req, res)=>{
         
     }
     res.status(200).render("contact.pug", params)
+})
+
+app.post("/contact", (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to the database")
+    }).catch(()=>{
+        res.status(400).send("Item was saved to the database")
+    })
+    // res.status(200).render("contact.pug")
 })
 
 // START THE SERVER
