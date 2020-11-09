7101e89f285de7e38b37490ba2bd2b7b3e1c3829
Shivang Mitra <shivangmitra8@gmail.com>
2020-11-09 14:48:16 +0530

    Created a Rest API using flask and Jsonify

---
+++ Projects/Python Projects/Rest API/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello"
+
+@app.route("/armstrong/<int:n>")
+def arms(n):
+    sum = 0
+    order = len(str(n))
+    copy_n = n
+    while (n > 0):
+        digit = n % 10
+        sum += digit ** order
+        n = n // 10
+
+    if (sum == copy_n):
+        result = {
+            "Number": copy_n,
+            "Armstrong": True
+        }
+    else:
+        result = {
+            "Number": copy_n,
+            "Armstrong": False
+        }
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
