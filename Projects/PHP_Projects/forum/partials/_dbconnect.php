1a5b86dfb4223453965dc0ebb112b1aba9fcc4b9
Shivang Mitra <shivangmitra8@gmail.com>
2020-11-17 19:26:51 +0530

    Started a forum website on php, created home and threads

---
+++ Projects/PHP_Projects/forum/partials/_dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "z_dizcuzz";
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+?>
---
+++ Projects/PHP_Projects/forum/partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="container-fluid bg-dark text-light">
+    <p class="text-center mb-0">Copyright zDizcuzz - Coding Forum 2020-21 | All rights reserved</p>
+</div>
