71b4b7da95320a1eeb406fe16aa9f5ab326b1e2c
Shivang Mitra <shivangmitra8@gmail.com>
2020-11-27 17:24:10 +0530

    Completed posts and like page of flutter app

--- Codes/Android_Development_Flutter/MARK_1/flutter_mark_1/lib/main.dart
+++ Codes/Android_Development_Flutter/MARK_1/flutter_mark_1/lib/main.dart
@@ -4,6 +4,25 @@ void main() {
   runApp(MyApp());
 }
 
+class Post{
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+
+  Post(this.body, this.author);
+
+  void likePost(){
+    this.userLiked = !this.userLiked;
+    if(this.userLiked){
+      this.likes += 1;
+    }
+    else{
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -31,18 +50,174 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget{
+// class MyHomePage extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(appBar: AppBar(title: Text("Hi Zephyrus")),
+//     body: TextInputWidget()
+//     );
+//     // body: Column(children: <Widget>[TestWidget(), TestWidget(), TestWidget()],//same goes for a Row
+//     // ));
+//   }
+// }
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Post> posts = [];
+
+  void newPost(String text){
+    this.setState(() {
+      posts.add(new Post(text, "Zephyrus"));
+    });
+  }
+
   @override
   Widget build(BuildContext context){
-    return Scaffold(appBar: AppBar(title: Text("Hi Zephyrus")),
-    body: Column(children: <Widget>[TestWidget(), TestWidget(), TestWidget()],//same goes for a Row
-    ));
+    return Scaffold(
+      appBar: AppBar(title: Text("Hi Zephyrus")),
+      body: Column(children: <Widget>[
+        Expanded(child: PostList(this.posts)),
+        TextInputWidget(this.newPost),
+      ]),
+    );
   }
 }
 
-class TestWidget extends StatelessWidget{
+// class TestWidget extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//     return Text("Hello...");
+//   }
+// }
+
+
+//stful
+class TextInputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  TextInputWidget(this.callback); // ()-requires ({})-optional named parameter ([])-optional unnamed parameter
+
   @override
-  Widget build(BuildContext context){
-    return Text("Hello...");
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+
+  // String text;
+
+  @override
+  void dispose(){
+    super.dispose();//clean the stuff in the widget when it has been used
+    controller.dispose();
+  }
+
+  // void changeText(text){
+  //   if(text == "chintu"){
+  //     controller.clear();
+  //     text = "";
+  //   }
+  //   setState(() {
+  //     this.text = text;// setState is to refresh and redraw the widget 
+  //   });
+  // }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(children: <Widget>[
+//       TextField(
+//         controller: this.controller,
+//         decoration: InputDecoration(
+//           prefixIcon: Icon(Icons.message), labelText: "Type a message:"),
+//           // onChanged: (text) => this.changeText(text),
+//       ),
+//       // Text(controller.text)
+//     ]);
+//   }
+// }
+
+
+  void click(){
+    widget.callback(controller.text);
+    controller.clear();
+    FocusScope.of(context).unfocus();
+  } 
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.message),
+          labelText: "Type a message",
+          suffixIcon: IconButton(
+            icon: Icon(Icons.send),
+            splashColor: Colors.deepOrange,
+            tooltip: "Post Message",
+            onPressed: this.click,
+          )
+        )
+      );
+  }
+}
+
+class PostList extends StatefulWidget {
+
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  _PostListState createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callback){
+    this.setState(() {
+      callback();
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context, index) {
+        var post = this.widget.listItems[index];
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ListTile(
+                  title: Text(post.body),
+                  subtitle: Text(post.author)
+                )
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      post.likes.toString(),
+                      style: TextStyle(fontSize: 20)
+                    ),
+                    padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.thumb_up),
+                    onPressed: () => this.like(post.likePost),
+                    color: post.userLiked ? Colors.blue : Colors.black
+                  )
+                ]
+              )
+            ]
+          )
+        );
+      },
+    );
   }
 }
