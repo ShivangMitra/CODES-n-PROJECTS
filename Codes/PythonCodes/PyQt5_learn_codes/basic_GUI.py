c4680f130d9cb9119f3ae7e60a3d6bd9d26b92c3
Shivang Mitra <shivangmitra8@gmail.com>
2020-11-30 16:32:12 +0530

    learned buttons and events in PyQt5

--- Codes/PythonCodes/PyQt5_learn_codes/basic_GUI.py
+++ Codes/PythonCodes/PyQt5_learn_codes/basic_GUI.py
@@ -2,15 +2,47 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(650, 200, 700, 500)
+        self.setWindowTitle("Zeph GUI")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Zephyrus no GUI")
+        self.label.move(300, 50)
+
+        self.btn1 = QtWidgets.QPushButton(self)
+        self.btn1.setText("Spank me!")
+        self.btn1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("aaah...harder...HARDER DADDY!!")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
+# def clicked():
+#     print("clicked")
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(650, 200, 700, 500)
-    win.setWindowTitle("Zeph GUI")
+    win = MyWindow()
+    # win.setGeometry(650, 200, 700, 500)
+    # win.setWindowTitle("Zeph GUI")
 
-    label = QtWidgets.QLabel(win)
-    label.setText("Zephyrus no GUI")
-    label.move(300, 50)
+    # label = QtWidgets.QLabel(win)
+    # label.setText("Zephyrus no GUI")
+    # label.move(300, 50)
+    #
+    # btn1 = QtWidgets.QPushButton(win)
+    # btn1.setText("Spank me!")
+    # btn1.clicked.connect(clicked)
 
     win.show()
     sys.exit(app.exec_())
