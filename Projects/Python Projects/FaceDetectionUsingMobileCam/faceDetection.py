1539f0a197b0a91eed8b9a3f62081681085a8ba8
Shivang Mitra <shivangmitra8@gmail.com>
2020-12-06 16:57:54 +0530

    Used phone cam for face recognition and blur

---
+++ Projects/Python Projects/FaceDetectionUsingMobileCam/faceDetection.py
@@ -0,0 +1,25 @@
+# https://github.com/opencv/opencv/tree/master/data/haarcascades
+
+import cv2, time
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+video = cv2.VideoCapture(0)
+address = "https://192.168.1.102:8080/video"
+video.open(address)
+
+while True:
+    check, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    for x, y, w, h in face:
+        img = cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 200, 0), 1)
+        img[y:y+h, x:x+w] = cv2.medianBlur(img[y:y+h, x:x+w], 35)
+
+    cv2.imshow("there you are", frame)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+video.release()
+cv2.destroyAllWindows()
