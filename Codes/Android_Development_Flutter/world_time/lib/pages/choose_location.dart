6b01d33b4d1011ff48a43537eae845cceddbec30
Shivang Mitra <shivangmitra8@gmail.com>
2021-02-04 20:53:52 +0530

    advanced some more in the world time app using flutter

--- Codes/Android_Development_Flutter/world_time/lib/pages/choose_location.dart
+++ Codes/Android_Development_Flutter/world_time/lib/pages/choose_location.dart
@@ -6,10 +6,50 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    //!gonna simulate a network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+    //!gonna simulate a network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return "weeb, introvert, gamer";
+    });
+
+    print("$username -- $bio");
+  }
+
+  // int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // print('initState function ran');
+
+    getData();
+    print("this is gonna run as getData is async func, so its non-blocking");
+  }
+
   @override
   Widget build(BuildContext context) {
+    // print('build function ran');
     return Scaffold(
-      body: Text("chosse location screen"),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a Location"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      // body: RaisedButton(
+      //   onPressed: () {
+      //     setState(() {
+      //       counter += 1;
+      //     });
+      //   },
+      //   child: Text("COUNTER IS: $counter"),
+      // ),
     );
   }
 }
