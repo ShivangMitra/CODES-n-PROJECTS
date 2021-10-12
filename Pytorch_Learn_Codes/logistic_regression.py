a68afcda589e5443f27cf163988ea802284accf8
Shivang Mitra <shivangmitra8@gmail.com>
2021-10-12 17:42:21 +0530

    Implemented Linear Regression using Pytorch

---
+++ Pytorch_Learn_Codes/logistic_regression.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+from sklearn.preprosessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
