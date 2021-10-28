f4e21b703cb5d90c8dcb375f1275a8325d5737eb
Shivang Mitra <shivangmitra8@gmail.com>
2021-10-28 21:22:47 +0530

    Created Face Detection Module

---
+++ Computer Vision/Face Detection/face_detection_min.py
@@ -0,0 +1,34 @@
+import cv2
+import mediapipe as mp
+import time
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
+
+cap = cv2.VideoCapture(0)
+ptime = 0
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+
+    if results.detections:
+        for id, detection in enumerate(results.detections):
+            # mpDraw.draw_detection(img, detection)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
+
+            cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+
+    ctime = time.time()
+    fps = 1/(ctime - ptime)
+    ptime = ctime
+
+    cv2.putText(img, f'FPS: {int(fps)}', (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
